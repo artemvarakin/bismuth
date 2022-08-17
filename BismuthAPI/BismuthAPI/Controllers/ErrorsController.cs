37b44eb5c4fb344434fa4e0fdde533a3af480649
artemvarakin <artemvarakinhxc@gmail.com>
2022-08-17 23:55:08 +0300

    feat: add global error handler

---
+++ BismuthAPI/BismuthAPI/Controllers/ErrorsController.cs
@@ -0,0 +1,15 @@
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BismuthAPI.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public sealed class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error() {
+        var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem(title: exception?.Message);
+    }
+}
