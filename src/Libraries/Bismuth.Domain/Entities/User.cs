ee18e5efdbe16a2b75b7e5d4e1289dc5e2ca36e8
artemvarakin <artemvarakinhxc@gmail.com>
2022-10-19 16:40:36 +0400

    feat: add domain lib

---
+++ src/Libraries/Bismuth.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace Bismuth.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+}
